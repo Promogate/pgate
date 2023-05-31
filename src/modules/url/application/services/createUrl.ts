c52be9db0baad782ecd36e9f2f3c8b0d5596fa55
Augusto Dias <gutodiasux@gmail.com>
2023-05-31 11:27:14 -0300

    feat: add CreateUrlService

---
+++ src/modules/url/application/services/createUrl.ts
@@ -0,0 +1,41 @@
+import { prisma } from '@/config/prisma';
+import { CreateUrl } from '@/modules/url/domain/features';
+import { nanoid } from 'nanoid';
+
+export class CreateUrlService implements CreateUrl {
+  async execute(input: CreateUrl.Input): Promise<CreateUrl.Ouput> {
+    let code = nanoid(8);
+
+    const codeAlreadyExists = await prisma.url.findUnique({
+      where: {
+        code: code
+      }
+    })
+
+    while (codeAlreadyExists) {
+      code = nanoid(8);
+    }
+
+    const result = await prisma.url.create({
+      data: {
+        code: code,
+        full_link: input.fullLink,
+        short_link: `https://pgate.app/${code}`,
+        offer_id: input.offerId,
+        store_name: input.storeName,
+        resource_id: input.resourceId,
+      }
+    })
+    
+    return {
+      id: result.id,
+      code: result.code,
+      resourceId: result.resource_id,
+      offerId: result.offer_id,
+      storeName: result.store_name,
+      fullLink: result.full_link,
+      shortLink: result.short_link,
+      createdAt: result.created_at
+    }
+  }
+}
