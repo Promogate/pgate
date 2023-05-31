be89167c003e4b7abc62d235bb2e63bd14581790
Augusto Dias <gutodiasux@gmail.com>
2023-05-31 11:28:34 -0300

    refact: change url creation to the service

--- src/app/create/route.ts
+++ src/app/create/route.ts
@@ -1,18 +1,13 @@
-import { prisma } from '@/config/prisma';
-import { nanoid } from 'nanoid';
+import { CreateUrlInput } from '@/modules/url/@types';
+import { CreateUrlService } from '@/modules/url/application/services';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  const body = await request.json() as { full_link: string };
+  const body = await request.json() as CreateUrlInput;
   
-  const code = nanoid(8);
+  const createUrlService = new CreateUrlService();
+  
+  const result = await createUrlService.execute(body)
 
-  const res = await prisma.url.create({
-    data: {
-      code,
-      full_link: body.full_link,
-      short_link: `https://pgate.app/${code}`
-    }
-  })
-  return NextResponse.json({ res });
+  return NextResponse.json({ data: result });
 }
