330f597c048f7b23d7875fe64899e5999b5e4afd
Augusto Dias <gutodiasux@gmail.com>
2023-06-30 11:19:14 -0300

    refact: change to RedisService

---
+++ src/modules/cache/application/RedisService.ts
@@ -0,0 +1,22 @@
+import Redis from 'ioredis';
+import { GetRedisCache, SetRedisCache } from '../features';
+
+export class RedisService implements SetRedisCache, GetRedisCache {
+  redis: Redis
+
+  constructor() {
+    this.redis = new Redis(process.env.UPSTASH_REDIS_URL as string);
+  }
+
+  async setCache(input: SetRedisCache.input): Promise<SetRedisCache.output> {
+    const content = JSON.stringify(input.content);
+    await this.redis.set(input.id, content, 'EX', 2678400);
+  }
+
+  async getCache(input: GetRedisCache.input): Promise<GetRedisCache.output> {
+    const cached = await this.redis.get(input.id);
+    if (!cached) return;
+    const result = JSON.parse(cached);
+    return result;
+  }
+}
