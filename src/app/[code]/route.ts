d6040853d601d4e82380b6693734d1a4a21c1d4e
Augusto Dias <gutodiasdev@gmail.com>
2023-09-22 17:10:41 -0300

    hotfix: api.promogate redirect

--- src/app/[code]/route.ts
+++ src/app/[code]/route.ts
@@ -1,28 +1,33 @@
-import { api } from '@/config';
 import { prisma } from '@/config/prisma';
-import { APIResponse, Params } from '@/modules/cache/@types';
-import { RedisService } from '@/modules/cache/application';
+import { Params } from '@/modules/cache/@types';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: Params) {
-  const redis = new RedisService();
-  const cached = await redis.getCache({ cacheKey: params.code});
-  if (!cached) {
-    const findURL = await prisma.url.findUnique({
-      where: {
-        code: params.code
-      }
-    })
-    console.log({findURL: findURL})
-    if (!findURL) return NextResponse.redirect('https://promogate.app/link-nao-encontrado');
-    const { data } = await api.get<APIResponse>(`/resources/offer`, {
-      headers: {
-        'x-short-link': findURL.short_link
-      }
-    });
-    console.log(data)
-    await redis.setCache({ cacheKey: params.code, content: data.offer })
-    return NextResponse.redirect(data.offer.destination_link);
-  }
-  return NextResponse.redirect(cached.destination_link);
+  // const redis = new RedisService();
+  // const cached = await redis.getCache({ cacheKey: params.code });
+  // console.time();
+  // if (!cached) {
+  // console.time();
+  const findURL = await prisma.url.findUnique({
+    where: {
+      code: params.code
+    }
+  });
+  console.log({ findURL: findURL });
+  if (!findURL) return NextResponse.redirect('https://promogate.app/link-nao-encontrado');
+  // const { data } = await api.get<APIResponse>(`/resources/offer`, {
+  //   headers: {
+  //     'x-short-link': findURL.short_link
+  //   }
+  // });
+  // console.log(data)
+  // await redis.setCache({ cacheKey: params.code, content: findURL });
+  // return NextResponse.redirect(data.offer.destination_link);
+  //   console.timeEnd();
+  //   console.log("passed throw api");
+  //   return NextResponse.redirect(`https://api.promogate.app/analytics/redirect/offer/${findURL.offer_id}`);
+  // }
+  // console.timeEnd();
+  // return NextResponse.redirect(cached.destination_link);
+  return NextResponse.redirect(`https://api.promogate.app/analytics/redirect/offer/with-query?shortLink=${findURL.short_link}`);
 }
