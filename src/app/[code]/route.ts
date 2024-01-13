9be933f7015875bed5251734e304f6e81b64b631
Augusto <gutodiasdev@gmail.com>
2024-01-13 18:03:52 -0300

    refact: remove redis cache from redirector retriver

--- src/app/[code]/route.ts
+++ src/app/[code]/route.ts
@@ -5,25 +5,25 @@ import { RedisService } from '@/modules/cache/application';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request, { params }: Params) {
-  const redis = new RedisService();
-  const cached = await redis.getCache({ cacheKey: params.code });
-  if (!cached) {
-    const urlData = await prisma.url.findUnique({
-      where: {
-        code: params.code
-      }
-    });
-    if (!urlData) return NextResponse.redirect('https://promogate.app/link-nao-encontrado');
-    if (urlData.type === "redirector") {
-      const { data } = await api.get(`/redirector/${urlData.redirector_id}`)
-      await redis.setCache({ cacheKey: params.code, content: data });
-      return NextResponse.redirect(data);
+  // const redis = new RedisService();
+  // const cached = await redis.getCache({ cacheKey: params.code });
+  // if (!cached) {
+  const urlData = await prisma.url.findUnique({
+    where: {
+      code: params.code
     }
-    if (urlData.type === "offer") {
-      const { data } = await api.get(`/offer/redirector/${urlData.redirector_id}`)
-      await redis.setCache({ cacheKey: params.code, content: urlData });
-      return NextResponse.redirect(data.offer.destination_link);
-    }
+  });
+  if (!urlData) return NextResponse.redirect('https://promogate.app/link-nao-encontrado');
+  if (urlData.type === "redirector") {
+    const { data } = await api.get(`/redirector/${urlData.redirector_id}`)
+    // await redis.setCache({ cacheKey: params.code, content: data });
+    return NextResponse.redirect(data);
   }
-  return NextResponse.redirect(cached.destination_link);
+  if (urlData.type === "offer") {
+    const { data } = await api.get(`/offer/redirector/${urlData.redirector_id}`)
+    // await redis.setCache({ cacheKey: params.code, content: urlData });
+    return NextResponse.redirect(data.offer.destination_link);
+  }
+  // }
+  // return NextResponse.redirect(cached.destination_link);
 }
